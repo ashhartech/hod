59e0cf9be6b7c9969dca50eabf0feaf886b242f4
ashhartech <ansco@users.noreply.github.com>
2020-07-24 13:11:17 +0300

    Initialize feature4719

---
+++ src/common/WixDistribution.h
@@ -0,0 +1,23 @@
+//-------------------------------------------------------------------------------------------------
+// <copyright file="WixDistribution.h" company="Outercurve Foundation">
+//   Copyright (c) 2004, Outercurve Foundation.
+//   This software is released under Microsoft Reciprocal License (MS-RL).
+//   The license and further copyright text can be found in the file
+//   LICENSE.TXT at the root directory of the distribution.
+// </copyright>
+// 
+// <summary>
+// Contains disribution specific items, such as Product Name
+// </summary>
+//-------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#ifndef __WIXDISTRIBUTION_FILE_H_
+#define __WIXDISTRIBUTION_FILE_H_
+
+#define VER_COMPANY_NAME    "FireGiant"
+#define VER_LEGAL_COPYRIGHT "Copyright (c) FireGiant and Outercurve Foundation.\240 All rights reserved."
+#define VER_PRODUCT_NAME    "FireGiant - WiX Toolset"
+
+#endif // __WIXDISTRIBUTION_FILE_H_
